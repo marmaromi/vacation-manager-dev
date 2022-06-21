a15bf98fd10352dd665ba59e00d70defb4fd3e40
marmaromi <marmaromi@gmail.com>
2022-06-21 17:49:02 +0300

    commit

--- Frontend/src/Components/VacationArea/AddVacation/AddVacation.tsx
+++ Frontend/src/Components/VacationArea/AddVacation/AddVacation.tsx
@@ -25,6 +25,7 @@ function AddVacation(): JSX.Element {
 
     async function send(vacation: VacationModel) {
         try {
+            validatePost(vacation);
             await vacationsService.addVacation(vacation);
             notifyService.success(`Vacation to ${vacation.destination} has been added successfully`);
             navigate("/vacations");
@@ -34,6 +35,12 @@ function AddVacation(): JSX.Element {
         }
     }
 
+    function validatePost(vacation: VacationModel): void {
+        const result = VacationModel.vacationPostValidationSchema.validate(vacation);        
+        // return result.error?.message;
+        console.log(result.error?.message)
+    }
+
     return (
         <div className="AddVacation Box">
             <h2>New Vacation</h2>
@@ -41,58 +48,62 @@ function AddVacation(): JSX.Element {
             <form onSubmit={handleSubmit(send)}>
 
                 <label>Destination: </label>
-                <input type="text" {...register("destination", {
+                <input type="text" {...register("destination")} />
+                {/* <input type="text" {...register("destination", {
                     required: { value: true, message: "Missing destination" },
                     // pattern: { value: /^[A-Za-z0-9]+$/, message: "Special Characters are not allowed" },
                     minLength: { value: 2, message: "Minimum 2 letters" },
                     maxLength: { value: 50, message: "Maximum 50 letters" },
                 })} />
-                <span>{errors.destination?.message}</span>
+                <span>{errors.destination?.message}</span> */}
                 <br />
 
                 <label>Description: </label>
-                <textarea rows={4} cols={20}  {...register("description", {
+                <textarea rows={4} cols={20}  {...register("description")} ></textarea>
+                {/* <textarea rows={4} cols={20}  {...register("description", {
                     required: { value: true, message: "Missing description" },
                     minLength: { value: 10, message: "Minimum 10 letters" },
                     maxLength: { value: 300, message: "Maximum 300 letters" },
                 })} ></textarea>
-                <span>{errors.description?.message}</span>
+                <span>{errors.description?.message}</span> */}
                 <br />
 
                 <label>Start Date: </label>
-                <input type="date" min={new Date().toISOString().slice(0, -14)} max={getValues("endDate")} {...register("startDate", {
+                <input type="date" min={new Date().toISOString().slice(0, -14)} max={getValues("endDate")} {...register("startDate")} />
+                {/* <input type="date" min={new Date().toISOString().slice(0, -14)} max={getValues("endDate")} {...register("startDate", {
                     required: { value: true, message: "Missing start date" },
                     min: { value: new Date().toISOString().slice(0, -14), message: "Start date must be today or later" },
                     max: { value: getValues("endDate"), message: "Start date must be today or later" }
                 })} />
-                <span>{errors.startDate?.message}</span>
+                <span>{errors.startDate?.message}</span> */}
                 <br />
 
                 <label>End Date: </label>
-                <input type="date" min={getValues("startDate") || new Date().toISOString().slice(0, -14)} {...register("endDate", {
+                <input type="date" min={getValues("startDate") || new Date().toISOString().slice(0, -14)} {...register("endDate")} />
+                {/* <input type="date" min={getValues("startDate") || new Date().toISOString().slice(0, -14)} {...register("endDate", {
                     required: { value: true, message: "Missing end date" },
                     min: { value: getValues("startDate"), message: "End date must be later than start date" }
                 })} />
-                <span>{errors.endDate?.message}</span>
+                <span>{errors.endDate?.message}</span> */}
                 <br />
 
                 <label>Price: </label>
-                <input type="number" {...register("price", {
+                <input type="number" {...register("price")} />
+                {/* <input type="number" {...register("price", {
                     required: { value: true, message: "Missing price" },
                     min: { value: 200, message: "Minimum price 200₪" },
                     max: { value: 100000, message: "Maximum price 100,000₪" },
                 })} />
-                <span>{errors.price?.message}</span>
+                <span>{errors.price?.message}</span> */}
                 <br />
 
                 <label>Image: </label>
-                <input type="file" accept="image/*" {...register("image", {
+                <input type="file" accept="image/*" {...register("image")} />
+                {/* <input type="file" accept="image/*" {...register("image", {
                     required: { value: true, message: "Missing image" },
                 })} />
-                <span>{errors.image?.message}</span>
+                <span>{errors.image?.message}</span> */}
                 <br />
-
-
 
                 <button>Add Vacation</button>
 
--- Frontend/src/Services/InterceptorService.ts
+++ Frontend/src/Services/InterceptorService.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { store } from "../Redux/Store";
 // import store from "../Redux/Store";
 import authService from "./AuthService";
 
@@ -6,13 +7,11 @@ class InterceptorService {
 
     public createInterceptors(): void {
 
-        // Send token for each request: 
         axios.interceptors.request.use(request => {
 
             if (authService.isLoggedIn()) {
                 request.headers = {
-                    // authorization: "Bearer " + store.getState().authState.token
-                    authorization: "Bearer " + localStorage.getItem("token")
+                    authorization: "Bearer " + store.getState().authStore.token
                 };
             }
 
