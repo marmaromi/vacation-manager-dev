297b2bb07ff2dac1c3bada548615d1cbc59b3849
marmaromi <marmaromi@gmail.com>
2022-06-28 17:53:01 +0300

    added foot notes

--- Frontend/src/Components/LayoutArea/Footer/Footer.tsx
+++ Frontend/src/Components/LayoutArea/Footer/Footer.tsx
@@ -3,7 +3,16 @@ import "./Footer.css";
 function Footer(): JSX.Element {
     return (
         <div className="Footer">
-			footer
+			<ul>
+                issues:
+                <li>surfing to a page other than main is not working properly</li>
+                <li>following a card doesn't reorder until page refresh, not even on logout/login </li>
+                <li>it's not really responsive for mobile</li>
+                <li> center the admin menu but keep th logout on the right</li>
+                <li>need to adjust footer size per content</li>
+                <li>add real content to the footer</li>
+                <li>upload to an external host</li>
+            </ul>
         </div>
     );
 }
