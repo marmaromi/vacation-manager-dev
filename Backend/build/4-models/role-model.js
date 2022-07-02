a7e0884ad227ed3be72a0fd12a13362345bac834
marmaromi <marmaromi@gmail.com>
2022-07-02 16:02:16 +0300

    first commit

--- Backend/build/4-models/role-model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Role;
-(function (Role) {
-    Role["User"] = "user";
-    Role["Admin"] = "admin";
-})(Role || (Role = {}));
-exports.default = Role;
